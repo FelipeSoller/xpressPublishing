b8ef558b39041b54e53ccb930df5a9fe19c08471
Felipe Soller <soller@outlook.com>
2020-08-01 00:21:45 -0300

    criando tabela de series

--- migration.js
+++ migration.js
@@ -3,10 +3,15 @@ const db = new sqlite3.Database('./database.sqlite');
 
 db.serialize(function() {
     db.run('CREATE TABLE IF NOT EXISTS `Artist` ( ' +
-    '`id` INTEGER NOT NULL, ' +
-    '`name` TEXT NOT NULL, ' +
-    '`date_of_birth` TEXT NOT NULL, ' +
-    '`biography` TEXT NOT NULL, ' +
-    '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
-    'PRIMARY KEY(`id`) )');
+        '`id` INTEGER NOT NULL, ' +
+        '`name` TEXT NOT NULL, ' +
+        '`date_of_birth` TEXT NOT NULL, ' +
+        '`biography` TEXT NOT NULL, ' +
+        '`is_currently_employed` INTEGER NOT NULL DEFAULT 1, ' +
+        'PRIMARY KEY(`id`) )');
+    db.run('CREATE TABLE IF NOT EXISTS `Series` ( ' +
+        '`id` INTEGER NOT NULL, ' +
+        '`name` TEXT NOT NULL, ' +
+        '`description` TEXT NOT NULL, ' +
+        'PRIMARY KEY(`id`) )');
 })
